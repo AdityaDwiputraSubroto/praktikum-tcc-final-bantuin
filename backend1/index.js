0c367b1219dc6a7be95b59b515ee128960000c5a
Adityas22 <adityasept2209@gmail.com>
2024-06-01 01:20:52 +0700

    perubahan

--- backend1/index.js
+++ backend1/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import SupplierRoute from './routes/SupplierRoute.js';
+import express from "express";
+import cors from "cors";
+import SupplierRoute from "./routes/SupplierRoute.js";
 
 const app = express();
 const PORT = 8080;
